b13c43f15405f4292e20f9415753c91530b8ad84
captainJack33 <qfhjack@gmail.com>
2022-03-28 17:33:06 -0700

    add gui interface

--- data.py
+++ data.py
@@ -1,104 +1,118 @@
-##Getting  the question data from https://opentdb.com/api_config.php
+# ##Getting  the question data from https://opentdb.com/api_config.php
 
-question_data = [
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In &quot;Super Mario 64&quot;, collecting 100 coins on a level will give you a 1-UP.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Film",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The 2010 film &quot;The Social Network&quot; is a biographical drama film about MySpace founder Tom Anderson.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The first Maxis game to feature the fictional language &quot;Simlish&quot; was The Sims (2000).",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "History",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "Adolf Hitler was accepted into the Vienna Academy of Fine Arts.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "General Knowledge",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "Haggis is traditionally ate on Burns Night.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Art",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "Venus of Willendorf is one of the earliest works of art, depicting the planets Mars and Venus embrace in the heavens at night.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "In &quot;Minecraft&quot;, gold tools are faster than diamond tools.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science & Nature",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "&quot;Tachycardia&quot; or &quot;Tachyarrhythmia&quot; refers to a resting heart-rate near or over 100 BPM.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Entertainment: Video Games",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Each piece in Tetris is called a tetris.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    }
-]
+import requests
+URL = "https://opentdb.com/api.php"
+PARAMETERS = {
+    "amount": 10,
+    "type": "boolean",
+}
+
+response = requests.get(url=URL, params=PARAMETERS)
+response.raise_for_status()
+data = response.json()
+question_data = data["results"]
+
+#
+# question_data = [
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In &quot;Super Mario 64&quot;, collecting 100 coins on a level will give you a 1-UP.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Entertainment: Film",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "The 2010 film &quot;The Social Network&quot; is a biographical drama film about MySpace founder Tom Anderson.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "The first Maxis game to feature the fictional language &quot;Simlish&quot; was The Sims (2000).",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "History",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "Adolf Hitler was accepted into the Vienna Academy of Fine Arts.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "General Knowledge",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "Haggis is traditionally ate on Burns Night.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Art",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "Venus of Willendorf is one of the earliest works of art, depicting the planets Mars and Venus embrace in the heavens at night.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "hard",
+#         "question": "In &quot;Minecraft&quot;, gold tools are faster than diamond tools.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Science & Nature",
+#         "type": "boolean",
+#         "difficulty": "medium",
+#         "question": "&quot;Tachycardia&quot; or &quot;Tachyarrhythmia&quot; refers to a resting heart-rate near or over 100 BPM.",
+#         "correct_answer": "True",
+#         "incorrect_answers": [
+#             "False"
+#         ]
+#     },
+#     {
+#         "category": "Entertainment: Video Games",
+#         "type": "boolean",
+#         "difficulty": "easy",
+#         "question": "Each piece in Tetris is called a tetris.",
+#         "correct_answer": "False",
+#         "incorrect_answers": [
+#             "True"
+#         ]
+#     }
+# ]
+#
